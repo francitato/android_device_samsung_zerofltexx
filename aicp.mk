8929435d1010b9eebb8e17e37a5d4ece730484bd
enesuzun2002 <4lukankalar@gmail.com>
2018-02-05 16:29:43 +0000

    Fix Bootanimation
    Change-Id: I509a1def2b34ab36ad22a1dd8d467d7f9c8300dd

--- aicp.mk
+++ aicp.mk
@@ -3,6 +3,7 @@ $(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
 
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
+-include vendor/aicp/configs/bootanimation.mk
 
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
@@ -16,3 +17,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_NAME := aicp_zerofltexx
 PRODUCT_DEVICE := zerofltexx
+
+
